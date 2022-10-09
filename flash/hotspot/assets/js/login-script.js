d47ae3881f42498be5ba817bf9cd9cb6ef3cec9a
Yvandenhende <yodi.vandenhende@nowjobs.com>
2022-10-09 19:47:47 +0200

    adjusting stylign

---
+++ flash/hotspot/assets/js/login-script.js
@@ -0,0 +1,13 @@
+const translations = [
+  { lang: 'en', key: '', value: '' },
+  { lang: 'nl', key: '', value: '' },
+  { lang: 'fr', key: '', value: '' },
+  { lang: 'de', key: '', value: '' },
+];
+const startLanguage = 'nl'
+
+onInit();
+
+function onInit() {
+  document.login.username.focus();
+}
