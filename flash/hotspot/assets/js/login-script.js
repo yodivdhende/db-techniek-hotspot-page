cb3b7684696bce87bee07a9b76876734dc359d87
Yvandenhende <yodi.vandenhende@nowjobs.com>
2022-10-12 15:49:07 +0200

    adding password toggle logic

--- flash/hotspot/assets/js/login-script.js
+++ flash/hotspot/assets/js/login-script.js
@@ -1,10 +1,19 @@
 const startLanguage = 'nl'
+let showPassword = false;
 
 onInit();
 
 function onInit() {
   document.login.username.focus();
   translateTo(startLanguage);
+}
+
+function togglePassword() {
+  console.log(`%c togglePassword view`, `background:yellow;color:black`, {  });
+  showPassword = !showPassword;
+  document.getElementById('pass').type = showPassword ? 'text': 'password';
+  document.getElementById("pass-toggle-visibility-off").classList.toggle('hide');
+  document.getElementById("pass-toggle-visibility-on").classList.toggle('hide');
 }
 
 function translateTo(targetLanguage) {
